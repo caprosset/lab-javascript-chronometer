706b4691bd86b0e36a09be04b9d588d8aecab7a5
caprosset <capucinerosset@gmail.com>
2019-10-29 20:01:48 +0100

    Complete first iteration

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,39 +1,47 @@
 // Constructor
-// function Chronometer() {
+function Chronometer() {    
+    this.currentTime = 0;
+    this.intervalId = '';
+}
 
-// }
+Chronometer.prototype.startClick = function () {
+    this.intervalId = setInterval( () => {
+        this.currentTime++;
+        this.setTime();
+    }, 1000);
+};
 
-// Chronometer.prototype.startClick = function () {
+Chronometer.prototype.setMinutes = function () {
+    var currentTimeInMin = Math.floor(this.currentTime / 60);
+    return currentTimeInMin;
+};
 
-// };
+Chronometer.prototype.setSeconds = function () {
+  var currentTimeInSec = this.currentTime % 60;
+  return currentTimeInSec;
+};
 
-// Chronometer.prototype.setMinutes = function () {
-  
-// };
+Chronometer.prototype.twoDigitsNumber = function (num) {
+    return ('0' + num).slice(-2);// returns a 0 before each number, and only keep the last 2 numbers
+};
 
-// Chronometer.prototype.setSeconds = function () {
-  
-// };
+Chronometer.prototype.setTime = function () {
+    var minutes = this.twoDigitsNumber(this.setMinutes());
+    var seconds = this.twoDigitsNumber(this.setSeconds());
 
-// Chronometer.prototype.twoDigitsNumber = function () {
-  
-// };
-
-// Chronometer.prototype.setTime = function () {
-
-// };
+};
 
 // Chronometer.prototype.setMilliseconds = function () {
 
 // };
 
-// Chronometer.prototype.stopClick = function () {
-  
-// };
+Chronometer.prototype.stopClick = function () {
+    clearInterval(this.intervalId);
+};
 
-// Chronometer.prototype.resetClick = function () {
-
-// };
+Chronometer.prototype.resetClick = function () {
+    this.currentTime = 0;
+};
 
 // Chronometer.prototype.splitClick = function () {
 
